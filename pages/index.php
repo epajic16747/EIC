062e74c1fd9d7682c52215ccd207f5677c6b8b94
epajic16747 <epajic2@etf.unsa.ba>
2016-12-31 02:02:22 +0100

    Tiho tise Toma

--- pages/index.php
+++ pages/index.php
@@ -3,7 +3,7 @@
 <HEAD>
 	<meta charset="utf-8">
 	<title>EIC</title>
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </HEAD>
 <BODY>	
 	<div class="main">
@@ -12,7 +12,7 @@
 		?>
 		<div class="site_content">
 			<div class="site_heading">
-			<p id="loginImage"><img src="../images/login.png" alt="Login image" align="right" height="3%" width="8.5%"></p>
+			<p id="loginImage"><img src="images/login.png" alt="Login image" align="right" height="3%" width="8.5%"></p>
 			<h1>EIC</h1>
 			<h2>Event Information Center</h2>
 			
@@ -22,19 +22,19 @@
 				<div class="menubar">
 	
 					<ul class="menu">
-						<li><a href="index.php">Home</a></li>
-						<li><a href="allEvents.php">All Events</a></li>
-						<li><a href="about.php">About</a></li>
-						<li><a href="contact.php">Contact</a></li>
+						<li><a href="pages/index.php">Home</a></li>
+						<li><a href="pages/allEvents.php">All Events</a></li>
+						<li><a href="pages/about.php">About</a></li>
+						<li><a href="pages/contact.php">Contact</a></li>
 						
 						<?php
 							if(isset($_SESSION['username']))
 				            {
 				                
-				                print '<li><a href="addEvent.php">Add Event</a></li>
-				                	   <li><a href="deleteEventPage.php">Delete Event</a></li>
-				                	   <li><a href="editEvent.php">Change Event</a></li>
-				                	   <li><a href="downloads.php">Downloads</a></li>';
+				                print '<li><a href="pages/addEvent.php">Add Event</a></li>
+				                	   <li><a href="pages/deleteEventPage.php">Delete Event</a></li>
+				                	   <li><a href="pages/editEvent.php">Change Event</a></li>
+				                	   <li><a href="pages/downloads.php">Downloads</a></li>';
 				            }
 						?>
 					</ul>
@@ -42,11 +42,11 @@
 			</div><!--Close header-->
 			<div class="banner_img">
 					<div class="hamo">
-					<img id="slide" src="../images/carousel/img1.jpg"  alt="banner">
+					<img id="slide" src="images/carousel/img1.jpg"  alt="banner">
 	
 					</div>
-					<div id="left_arrow"><img onClick="Carousel(-1)" class="left" src="../images/left-arrow-gray-hi.png"/></div>
-					<div id="right_arrow"><img  onClick="Carousel(1)" class="right" src="../images/left-arrow-right-hi.png"/></div>
+					<div id="left_arrow"><img onClick="Carousel(-1)" class="left" src="images/left-arrow-gray-hi.png"/></div>
+					<div id="right_arrow"><img  onClick="Carousel(1)" class="right" src="images/left-arrow-right-hi.png"/></div>
 			</div><!--Close banner image(slider)-->
 			    
 
@@ -57,7 +57,7 @@
 		        if(isset($_SESSION['username']))
 		            { 
 		                
-		                print '<center><h2>You are already logged in.You can only logout at this moment.</h1><br><h2>Do you want to <a href="logout.php">logout</a> ?</h1></center>'; 
+		                print '<center><h2>You are already logged in.You can only logout at this moment.</h1><br><h2>Do you want to <a href="pages/logout.php">logout</a> ?</h1></center>'; 
 
 		            }
 	            else
@@ -65,9 +65,9 @@
 		                
 			            print '
 					        <center>
-								<form method="POST" action="login.php" style="background-color:#5b5e63" id="loginID">
+								<form method="POST" action="pages/login.php" style="background-color:#5b5e63" id="loginID">
 								  <div class="imgcontainer">
-								    <img src="../images/loginFormImage.png" alt="Avatar" class="loginImage">
+								    <img src="images/loginFormImage.png" alt="Avatar" class="loginImage">
 								  </div>
 
 								  <div class="container">
@@ -97,7 +97,7 @@
 						
 						<div class="event_image">
 						<div class="column one">
-							<img src="../images/no-image.png"/>
+							<img src="images/no-image.png"/>
 						</div>
 						</div>
 						<div class="main_info">
@@ -121,7 +121,7 @@
 						
 						<div class="event_image">
 						<div class="column one">
-							<img src="../images/no-image.png"/>
+							<img src="images/no-image.png"/>
 						</div>
 						</div>
 						<div class="main_info">
@@ -139,17 +139,17 @@
 					</div>
 				</div><!--close event-->
 				<!--<script type="text/javascript" src="../js/ajaxPageLoad.js"></script>-->
-				<script type="text/javascript" src="../js/slider.js"></script>
-				<script type="text/javascript" src="../js/addEventLocalStorage.js"></script>
-				<script type="text/javascript" src="../js/contactLocalStorage.js"></script>
-				<script type="text/javascript" src="../js/validacijaAddEvent.js"></script>
-				<script type="text/javascript" src="../js/contactValidacijaForme.js"></script>
-				<script type="text/javascript" src="../js/login.js"></script>
+				<script type="text/javascript" src="js/slider.js"></script>
+				<script type="text/javascript" src="js/addEventLocalStorage.js"></script>
+				<script type="text/javascript" src="js/contactLocalStorage.js"></script>
+				<script type="text/javascript" src="js/validacijaAddEvent.js"></script>
+				<script type="text/javascript" src="js/contactValidacijaForme.js"></script>
+				<script type="text/javascript" src="js/login.js"></script>
 
 			</div><!--close siteContent-->
 		</div>
 		<div class="footer">  
-			<a href=index.html >WT project v3.0 Spirala I </a> 
+			<a href=pages/index.php>WT project v3.0 Spirala I </a> 
 		</div><!--close footer-->	
 	</div>		
 
